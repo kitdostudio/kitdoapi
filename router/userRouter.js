efcea27e63539e4e584f3e7f54be530c9893f501
Kit Đỗ Studio <kitdommo>
2021-06-08 12:08:38 +0700

    update my code

--- router/userRouter.js
+++ router/userRouter.js
@@ -51,7 +51,8 @@ userrouter.get('/', function (req, res) {
     var datetime_out = getata.date;
     var now = new Date().toLocaleString("en-US", {timeZone: "Asia/Ho_Chi_Minh"});
      var datetime_now = dateFormat(now, "h:MM:ss dd-mm-yyyy");
-     var now_days = Date.parse(datetime_now);
+     var now_days = new Date(Date.parse(datetime_now));
+     var out_days = new Date(Date.parse(datetime_out));
     //var span = timeSpan.FromDates(datetime_now,datetime_out);
     var token = jwt.sign(getata.username + getata.password + getata.key + random.int(12345, 99999), 'login');
     if (listtoken.has_user(req.query.username)) {
@@ -64,7 +65,8 @@ userrouter.get('/', function (req, res) {
         message: 'Login thanh cong',
         payload: {
             token: token,
-            time: now_days
+            time: now_days,
+            timeout : out_days
         }
     });
 });
